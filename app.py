ba810e8886e3f224d6e0a1ec058b6a60f29d052d
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-07-02 10:26:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,7 @@ st.title(" Excel Data Statistical Analyzer")
 uploaded_file = st.file_uploader("Upload an Excel or CSV file", type=["csv", "xlsx"])
 
 if uploaded_file.name.endswith('.csv'):
-    try:
-        df = pd.read_csv(uploaded_file)
-    except UnicodeDecodeError:
-        # Try fallback encoding if UTF-8 fails
-        df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
-    except Exception as e:
-        st.error(f"Error reading CSV: {e}")
-        st.stop()
-    else:
-        df = pd.read_excel(uploaded_file)
+    df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
     df.rename(columns={
     "Mechanic 's response on leakage type": "Leak Type",
     "Plant Name": "Plant",
