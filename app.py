7cfb6e39153dd0831b4158ec8aacc49f20667745
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-07-02 10:30:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title(" Excel Data Statistical Analyzer")
 
 uploaded_file = st.file_uploader("Upload an Excel or CSV file", type=["csv", "xlsx"])
 
-if uploaded_file.name.endswith('.csv'):
+if uploaded_file:
     df = pd.read_csv(uploaded_file, encoding='ISO-8859-1')
     df.rename(columns={
     "Mechanic ‘s response on leakage type": "Leak Type",
