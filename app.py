0fb636e604461941986ccaae4fe012ea3ffab84a
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-07-03 11:27:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,23 +53,7 @@ if uploaded_file:
             )
             bar_fig.update_layout(xaxis_tickangle=-45)
             st.plotly_chart(bar_fig, use_container_width=True)
-        else :
-            st.subheader("Plant–wise Total Complaints")
-            plant_count=fil_df["Plant"].value_counts().reset_index()
-            plant_count.columns = ["Plant", "Total Complaints"]
-                            
-            bar_fig = px.bar(
-            plant_count,
-            x="Plant",
-            y="Total Complaints",
-            title="Total Complaints by Plant",
-            text="Total Complaints",
-            color="Total Complaints",
-            color_continuous_scale="blues"
-            )
-            bar_fig.update_layout(xaxis_tickangle=-45)
-            st.plotly_chart(bar_fig, use_container_width=True) 
-
+        else :  
             if sel_plant:
                 st.subheader("Leak–wise Total Complaints")
                 leak_c=fil_df["Leak Type"].value_counts().reset_index()
@@ -86,6 +70,22 @@ if uploaded_file:
                 )
                 bar_fig.update_layout(xaxis_tickangle=-45)
                 st.plotly_chart(bar_fig, use_container_width=True)
+            else:
+                st.subheader("Plant–wise Total Complaints")
+                plant_count=fil_df["Plant"].value_counts().reset_index()
+                plant_count.columns = ["Plant", "Total Complaints"]
+                                
+                bar_fig = px.bar(
+                plant_count,
+                x="Plant",
+                y="Total Complaints",
+                title="Total Complaints by Plant",
+                text="Total Complaints",
+                color="Total Complaints",
+                color_continuous_scale="blues"
+                )
+                bar_fig.update_layout(xaxis_tickangle=-45)
+                st.plotly_chart(bar_fig, use_container_width=True) 
         
         dist_count=fil_df["Distributor Name"].value_counts().reset_index()
         dist_count.columns = ["Distributor Name", "Total Complaints"]
