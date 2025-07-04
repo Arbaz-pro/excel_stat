a057e436ac4e0993e8c623dd9897c43bbfb200a8
Arbaz-pro <arbazpawaskar2@gmail.com>
2025-07-04 14:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,9 +39,10 @@ elif st.session_state.page == "analyze":
 
 #add dropdowns to filter data
     col1, col2, col3 = st.columns(3)
-    with col1:      
+    with col1: 
         state_options =["ALL"] + sorted(ndf["State Office"].dropna().unique())
-        sel_state = st.multiselect("State Office",state_options,default="ALL")
+        sel_state = st.multiselect("State Office",state_options)
+        set.write("test",sel_state)
     fil_df = ndf.copy()
     if "ALL" in sel_state:
          fil_df=fil_df[fil_df["State Office"].isin(state_options)]
